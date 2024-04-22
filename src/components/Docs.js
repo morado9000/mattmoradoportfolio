bac3c38e638bce6518161598aedbe726bfcc72d2
morado9000 <matthewmorado@gmail.com>
2024-04-21 19:11:25 -0700

    Updated link

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -9,7 +9,7 @@ export default function Docs () {
                 <div className="container flex flex-col justify-center space-y-10 mb-32 mx-auto" x-intersect="$el.classList.add('fadeInUp')">
                     <div className="flex flex-col items-center mx-auto md:w-1/2">
                         <h2 className="text-4xl font-bold text-center">Resume</h2>
-                        <a className="w-24 p-5 m-3 bg-black text-center text-white" href="Matthew_Morado_Resume_2023.pdf" target="_blank">Go</a>
+                        <a className="w-24 p-5 m-3 bg-black text-center text-white" href="Matthew_Morado_Resume_2023-1-3.pdf" target="_blank">Go</a>
                     </div>
                     <div className="flex flex-col items-center mx-auto md:w-1/2">
                         <h2 className="text-4xl font-bold text-center">Diploma</h2>
