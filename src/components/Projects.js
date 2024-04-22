45ecf393ae3e3ca7503ba7b8c7428e9eb93f2a4b
morado9000 <matthewmorado@gmail.com>
2024-04-21 18:58:51 -0700

    New resume and project entry

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,19 @@ export default function Projects() {
                       Projects
             </h2>
           <div className="container flex flex-col justify-center items-center mx-auto mb-32 md:space-x-20 md:items-start md:space-y-0 w-full md:flex-row">
+             <div className="flex flex-col justify-center items-center space-y-10 max-w-sm md:justify-start md:w-1/2" x-intersect="$el.classList.add('scale')">
+                  <h2 className="text-4xl font-bold text-center">
+                      Rubios Home Page
+                  </h2>
+                  <img src={require("../img/rubiosmaterialscreen.png")} alt="" />
+                  <button className="w-24 p-5 m-3 bg-black text-white">
+                        <a href="https://morado9000.github.io/rubios-material/">Go</a>
+                    </button>
+                  <p className="text-center text-gray-400">
+                      A recreation of the Rubios hompage. My goal was to implement a UI design utilized in the real world. Built with MaterialUI in React to execute the page's reponsive design.
+                  </p>
+                </div>
+
               <div className="flex flex-col justify-center items-center space-y-10 max-w-sm md:justify-start md:w-1/2" x-intersect="$el.classList.add('scale')">
                   <h2 className="text-4xl font-bold text-center">
                       Movie Ticketing
@@ -16,19 +29,6 @@ export default function Projects() {
                     </button>
                   <p className="text-center text-gray-400">
                       A website that simulates a movie ticketing site that allows the user to buy tickets for a certain movie. And admin can login to add or delete entries using ui. Frontend built with React, Redux, and Tailwind. Backend is made with Spring Boot and Node.js, and I'm using PostgreSQL for a database. Hosted on AWS with ECS and EFS. To get current movie data, I'm using TheMovieDB API.
-                  </p>
-                </div>
-
-                <div className="flex flex-col justify-center items-center space-y-10 max-w-sm md:justify-start md:w-1/2" x-intersect="$el.classList.add('scale')">
-                  <h2 className="text-4xl font-bold text-center">
-                      Video Game Search
-                  </h2>
-                  <img src={require("../img/ScreenshotVideoGame.png")} alt="" />
-                  <button className="w-24 p-5 m-3 bg-black text-white">
-                        <a href="http://video-loadb-14mu2ua08yv82-146d29c0ec1f0ebd.elb.us-west-1.amazonaws.com/">Go</a>
-                    </button>
-                  <p className="text-center text-gray-400">
-                      Do you just want a simple search app to get the latest video games? This website does just that and only that. Frontend is made with React, Redux, and Tailwind. Using Node.js as a backend. Host on AWS with ECS. Using the IGDB API to get video game data.
                   </p>
                 </div>
   
